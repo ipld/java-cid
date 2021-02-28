d7cba0bf4e6224afea53bfe64d09e238541368a9
crick77 <38907408+crick77@users.noreply.github.com>
2021-02-28 16:33:46 +0100

    Update Cid.java
    Added support for Libp2p-key (codec code 0x72) decoding of IPNS key (tested on go-ipfs version 0.8.0).

--- src/main/java/io/ipfs/cid/Cid.java
+++ src/main/java/io/ipfs/cid/Cid.java
@@ -19,6 +19,7 @@ public class Cid extends Multihash {
         Raw(0x55),
         DagProtobuf(0x70),
         DagCbor(0x71),
+        Libp2pKey(0x72), // support for IPNS key (go-ipfs 0.8.0)
         EthereumBlock(0x90),
         EthereumTx(0x91),
         BitcoinBlock(0xb0),
