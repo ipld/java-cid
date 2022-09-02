c6bfaa072092efe00aeaa464bebbf28f7360f4ee
ian <ianopolous@protonmail.com>
2022-09-02 20:34:06 +0100

    Fix test

--- src/test/java/io/ipfs/cid/CidTest.java
+++ src/test/java/io/ipfs/cid/CidTest.java
@@ -11,11 +11,11 @@ import java.util.*;
 public class CidTest {
 
     @Test
-    public void validStrings() throws IOException {
+    public void validStrings() {
         List<String> examples = Arrays.asList(
                 "QmPZ9gcCEpqKTo6aq61g2nXGUhM4iCL3ewB6LDXZCtioEB",
                 "QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy",
-                "zdpuAyvkgEDQm9TenwGkd5eNaosSxjgEYd8QatfPetgB1CdEZ"
+                "bafyreigivjmlrue5db7rpwmbonv7oq57hvnp7yzhlsoy3fbwi5jzhwgali"
         );
         for (String example: examples) {
             Cid cid = Cid.decode(example);
